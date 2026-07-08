894c84fc7a5133a395913f1647a495f5bcbb2014
Chia-en Tseng <ann.c.tseng@gmail.com>
2026-07-07 18:18:52 -0700

    Worked on base bar chart for Scene 2

--- script.js
+++ script.js
@@ -72,6 +72,47 @@ d3.csv("data/vgsales.csv").then(data => {
         svg.append("path").datum(annualSales).attr("fill", "none").attr("stroke", "black").attr("stroke-width", 2.5).attr("d", lineGenerator);
     }
 
-    drawScene1(cleanedData);
+    function drawScene2(cleanedData) {
+        const platformSales = d3.rollups(
+            cleanedData,
+            games => d3.sum(games, d => d.Global_Sales),
+            d => d.Platform
+        )
+        .map(d => ({
+            platform: d[0],
+            sales: d[1]
+        }))
+        .sort((a,b) => b.sales - a.sales)
+        .slice(0,10);
+
+        console.log("Platform sales:", platformSales);
+
+        const width = 900;
+        const height = 450;
+        const margin = {
+            top: 40,
+            right: 40,
+            bottom: 60,
+            left: 70
+        };
+
+        d3.select('#chart').html("");
+
+        const svg = d3.select("#chart").append("svg").attr("viewBox", `0 0 ${width} ${height}`).attr("width", "100%").attr("height", "100%");
+
+        const xScale = d3.scaleBand().domain(platformSales.map(d => d.platform)).range([margin.left, width - margin.right]).padding(0.5);
+        const yScale = d3.scaleLinear().domain([0, d3.max(platformSales, d => d.sales)]).nice().range([height - margin.bottom, margin.top]);
+
+        const xAxis = d3.axisBottom(xScale);
+        const yAxis = d3.axisLeft(yScale);
+
+        svg.append("g").attr("transform", `translate(0, ${height - margin.bottom})`).call(xAxis);
+        svg.append("g").attr("transform", `translate(${margin.left}, 0)`).call(yAxis);
+
+        svg.selectAll("rect").data(platformSales).enter().append("rect").attr("x", d => xScale(d.platform)).attr("y", d => yScale(d.sales)).attr("width", xScale.bandwidth()).attr("height", d => height - margin.bottom - yScale(d.sales)).attr("fill", "grey");
+    }
+
+    //drawScene1(cleanedData);
+    //drawScene2(cleanedData);
 
 });
