1648c7fe281033e45ce2a6f38304da43e7bdcf98
Chia-en Tseng <ann.c.tseng@gmail.com>
2026-07-08 20:06:00 -0700

    Added annotations, interaction triggers, and narrative explanations per scene

--- graphs.js
+++ graphs.js
@@ -17,8 +17,10 @@ d3.csv("data/vgsales.csv").then(data => {
     }));
 
     function drawScene1(cleanedData) {
+        //Years 2017 and beyond have not much global sales data to support. So cut those years for a more accurate graph.
+        const scene1Data = cleanedData.filter(d => d.Year <= 2016);
         const annualSales = d3.rollups(
-            cleanedData,
+            scene1Data,
             games => d3.sum(games, d => d.Global_Sales),
             d => d.Year
         )
@@ -28,7 +30,7 @@ d3.csv("data/vgsales.csv").then(data => {
         }))
         .sort((a,b) => a.year - b.year);
 
-        console.log("Annual sales:", annualSales);
+        console.log("Annual sales up to 2016, inclusive:", annualSales);
 
         const width = 900;
         const height = 450;
@@ -57,6 +59,35 @@ d3.csv("data/vgsales.csv").then(data => {
 
         const lineGenerator = d3.line().x(d => xScale(d.year)).y(d => yScale(d.sales));
         svg.append("path").datum(annualSales).attr("fill", "none").attr("stroke", "black").attr("stroke-width", 2.5).attr("d", lineGenerator);
+    
+        //Annotation
+        const peak = annualSales.reduce((a,b) => b.sales > a.sales ? b:a);
+
+        svg.append("circle").attr("cx", xScale(peak.year)).attr("cy", yScale(peak.sales)).attr("r", 5).attr("fill", "red");
+        svg.append("line").attr("x1", xScale(peak.year)).attr("y1", yScale(peak.sales)).attr("x2", xScale(2011)).attr("y2", yScale(700)).attr("stroke", "red").attr("stroke-width", 2);
+        svg.append("text").attr("x", xScale(2009)).attr("y", yScale(710)).attr("class", "annotation-text").text(`Global sales peaked in ${peak.year}`);
+
+        //Hover tooltip
+        let tooltip = d3.select(".tooltip");
+        if(tooltip.empty()) {
+            tooltip = d3.select("body").append("div").attr("class", "tooltip");
+        }
+        svg.selectAll(".hover-point").data(annualSales).enter().append("circle").attr("class", "hover-point").attr("cx", d=> xScale(d.year)).attr("cy", d => yScale(d.sales)).attr("r", 4).attr("fill", "white").attr("stroke", "black").attr("stroke-width", 1.5)
+            .on("mouseover", function(event, d) {
+                d3.select(this.parentNode).append("circle").attr("class", "hover-highlight").attr("cx", xScale(d.year)).attr("cy", yScale(d.sales)).attr("r", 4).attr("fill", "red").style("pointer-events", "none").attr("stroke", "black").attr("stroke-width", 1.5);
+
+                tooltip.style("display", "block").html(`
+                    <strong>${d.year}</strong><br>
+                    Global Sales: ${d.sales.toFixed(2)} million copies sold
+                `);
+            })
+            .on("mousemove", function(event) {
+                tooltip.style("left", `${event.pageX + 14}px`).style("top", `${event.pageY - 28}px`);
+            })
+            .on("mouseout", function() {
+                svg.selectAll(".hover-highlight").remove();
+                tooltip.style("display", "none");
+            });
     }
 
     function drawScene2(cleanedData) {
@@ -100,6 +131,32 @@ d3.csv("data/vgsales.csv").then(data => {
         svg.append("text").attr("transform", "rotate(-90)").attr("x", -height/2).attr("y", 20).attr("text-anchor", "middle").attr("class", "axis-label").text("Total Global Sales (millions of copies sold)");
 
         svg.selectAll("rect").data(platformSales).enter().append("rect").attr("x", d => xScale(d.platform)).attr("y", d => yScale(d.sales)).attr("width", xScale.bandwidth()).attr("height", d => height - margin.bottom - yScale(d.sales)).attr("fill", "grey");
+        
+        //Annotation
+        const bestPlatform = platformSales[0];
+        svg.append("circle").attr("cx", xScale(bestPlatform.platform) + xScale.bandwidth()/2).attr("cy", yScale(bestPlatform.sales)).attr("r", 4).attr("fill", "red");
+        svg.append("line").attr("x1", xScale(bestPlatform.platform) + xScale.bandwidth()/2).attr("y1", yScale(bestPlatform.sales)).attr("x2", 250).attr("y2", 30).attr("stroke", "red").attr("stroke-width", 2); 
+        svg.append("text").attr("x", 255).attr("y", 35).attr("class", "annotation-text").text("PlayStation 2 dominated global sales");
+        
+        //Hover tooltip
+        let tooltip = d3.select(".tooltip");
+        if (tooltip.empty()) {
+            tooltip = d3.select("body").append("div").attr("class", "tooltip");
+        }
+
+        svg.selectAll("rect").on("mouseover", function(event, d) {
+            d3.select(this).attr("fill", "red");
+
+            tooltip.style("display", "block").html(`
+                <strong>${d.platform}</strong><br>
+                Global Sales: ${d.sales.toFixed(2)} million copies sold
+            `);
+        }).on("mousemove", function(event) {
+                tooltip.style("left", `${event.pageX + 14}px`).style("top", `${event.pageY - 28}px`);
+        }).on("mouseout", function() {
+            d3.select(this).attr("fill", "grey");
+            tooltip.style("display", "none");
+        });
     }
 
     function drawScene3(cleanedData, regionColumn = "NA_Sales") {
@@ -162,6 +219,40 @@ d3.csv("data/vgsales.csv").then(data => {
 
 
         svg.selectAll("rect").data(genreSales).enter().append("rect").attr("x", d => xScale(d.genre)).attr("y", d => yScale(d.sales)).attr("width", xScale.bandwidth()).attr("height", d => height - margin.bottom - yScale(d.sales)).attr("fill", "grey");
+    
+        //Annotation
+        const topGenre = genreSales[genreSales.length - 1];
+        const annotationText = {
+            NA_Sales: "Action games were the most popular genre for North American sales",
+            EU_Sales: "Action games were the most popular genre for European sales",
+            JP_Sales: "Role-Playing games were the most popular genre for Japanese sales",
+            Other_Sales: "Action games were the most popular genre for other regions"
+        };
+        svg.append("circle").attr("cx", xScale(topGenre.genre) + xScale.bandwidth()/2).attr("cy", yScale(topGenre.sales)).attr("r", 5).attr("fill", "red");
+        svg.append("line").attr("x1", xScale(topGenre.genre) + xScale.bandwidth()/2).attr("y1", yScale(topGenre.sales)).attr("x2", width - 330).attr("y2", 40).attr("stroke", "red").attr("stroke-width", 2);
+        svg.append("text").attr("x", width - 480).attr("y", 32).attr("class", "annotation-text").text(annotationText[regionColumn]);
+    
+        //Hover tooltip
+        let tooltip = d3.select(".tooltip");
+
+        if (tooltip.empty()) {
+            tooltip = d3.select("body").append("div").attr("class", "tooltip");
+        }
+
+        svg.selectAll("rect").on("mouseover", function(event, d) {
+            d3.select(this).attr("fill", "red");
+            tooltip.style("display", "block").html(`
+                <strong>${d.genre}</strong><br>
+                ${regionLabels[regionColumn]} sales: ${d.sales.toFixed(2)} million copies sold
+            `);
+        })
+        .on("mousemove", function(event) {
+            tooltip.style("left", `${event.pageX + 14}px`).style("top", `${event.pageY - 28}px`);
+        })
+        .on("mouseout", function() {
+            d3.select(this).attr("fill", "grey");
+            tooltip.style("display", "none");
+        });
     }
 
     cleanedDataGlobal = cleanedData;
--- navigation.js
+++ navigation.js
@@ -9,20 +9,50 @@ function showScene(sceneNumber) {
 
     if(currentScene === 1) {
         d3.select("#scene-title h2").text("Scene 1");
-        d3.select("#scene-title h3").text("How did video games become a global industry?");
+        d3.select("#scene-title h3").text("How did physical console game sales grow worldwide?");
         drawScene1(cleanedDataGlobal);
+        d3.select("#annotations").html(`
+            <h3>Narrative Insight Annotation:</h3>
+            <p> Global physical console video game sales reached their highest annual total around 2008, 
+            marking the peak of the physical console era before annual sales began to decline.
+            While the dataset focuses on physical console game sales, the industry has continued to evolve. 
+            Mobile and PC gaming have become increasingly important, changing how games are purchased and contribute
+            to the decline.
+            </p>
+            <p><strong>Interaction:</strong> Hover over points on the line to see the exact year and global sales value.</p>
+        `);
     }
 
     if(currentScene === 2) {
         d3.select("#scene-title h2").text("Scene 2");
         d3.select("#scene-title h3").text("Which platforms powered the growth?");
         drawScene2(cleanedDataGlobal);
+        d3.select("#annotations").html(`
+            <h3>Narrative Insight Annotation:</h3>
+            <p> 
+            The PlayStation 2 (PS2) is the best-selling and most popular video game console of all time.
+            This illustrates how a few dominant consoles were responsible for much of the industry's worldwide growth. 
+            The top platforms shown here represent some of the most influential gaming systems during the physical 
+            console era.
+            </p>
+            <p><strong>Interaction:</strong> Hover over each bar to view its total worldwide game sales. </p>
+        `);
     }
 
     if(currentScene === 3) {
         d3.select("#scene-title h2").text("Scene 3");
         d3.select("#scene-title h3").text("How did genre preferences differ across world regions?");
         drawScene3(cleanedDataGlobal);
+        d3.select("#annotations").html(`
+            <h3>Narrative Insight Annotation:</h3><p> 
+            Most regions favored Action games, while Japan uniquely favored Role-Playing games. 
+            This contrast highlights how regional gaming preferences shaped the global video game market. 
+            </p>
+            <p><strong>Interaction:</strong> 
+            Select a region from the dropdown to compare genre preferences across different world regions. 
+            Hover over each bar to view the exact game sales. 
+            </p>
+        `);
     }
 
     updateButtonsVisibility();
